84293676c737ad6aa887ce24f071d0af1965681d
momazia@gmail.com <momazia@gmail.com>
2016-01-26 12:10:37 -0800

    Added the template + the main code

--- src/exercise/template.with.conditional.logic/main.go
+++ src/exercise/template.with.conditional.logic/main.go
@@ -6,7 +6,27 @@ import (
 	"text/template"
 )
 
-func main() {
-	template.ParseFiles("conditional.logic.temp")
+const condition bool = true // Constant used to show difference results using the template.
 
+func main() {
+
+	//Parsing the template
+	temp, err := template.ParseFiles("src/exercise/template.with.conditional.logic/conditional.logic.temp.html")
+
+	// Logging possible errors
+	logError(err)
+
+	// Executing the template using the constant
+	err = temp.Execute(os.Stdout, condition)
+
+	// Logging possible errors
+	logError(err)
+}
+
+// Logs error
+func logError(err error) {
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
