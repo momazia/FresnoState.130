2ee74663f0fe5c33f86e3041653441bc669df073
momazia@gmail.com <momazia@gmail.com>
2016-01-26 18:54:35 -0800

    Challenge 1

---
+++ src/exercise/challenges/main.go
@@ -0,0 +1,14 @@
+/*
+	Source: 		Column AP in Gradebook
+	Author: 		Mohamad Mahdi Ziaee
+	Description:	1) Create a program which prints “Hello World” to the terminal.
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Hello world!")
+}
