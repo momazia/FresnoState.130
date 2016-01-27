3c5f23a3ceda5c59174c63346f9a20c31e23774b
momazia@gmail.com <momazia@gmail.com>
2016-01-26 18:57:30 -0800

    2nd challenge.

---
+++ src/exercise/challenges/challenge2/main.go
@@ -0,0 +1,15 @@
+/*
+	Source: 		Column AP in Gradebook
+	Author: 		Mohamad Mahdi Ziaee
+	Description:	2) Modify the previous program so that instead of printing Hello World it prints Hello,
+					my name is followed by your name.
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Hello, my name is Mahdi")
+}
