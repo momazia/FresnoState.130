5b0f3449b50d246269c74c8a23f942b44022e16d
momazia@gmail.com <momazia@gmail.com>
2016-01-26 20:41:50 -0800

    2nd challenge 4 completed

---
+++ src/exercise/challenges/2ndchallenge4/main.go
@@ -0,0 +1,16 @@
+/*
+	Source: 		Column AP in Gradebook
+	Author: 		Mohamad Mahdi Ziaee
+	Description:	4) What's the value of this expression: (true && false) || (false && true) || !(false && false)?
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("Result: ", (true && false) || (false && true) || !(false && false))
+
+}
