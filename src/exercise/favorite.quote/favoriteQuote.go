84d8598c19fd3a86600c89bf9dbac44b34a8caf7
momazia@gmail.com <momazia@gmail.com>
2016-02-09 15:00:54 -0800

    Added an extra space for quotation

--- src/exercise/favorite.quote/favoriteQuote.go
+++ src/exercise/favorite.quote/favoriteQuote.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-const QUOTE string = "A man's enjoyment of all good things is in exact proportion to the pains he has undergone to gain them. \n - Cyrus the Great"
+const QUOTE string = "A man's enjoyment of all good things is in exact proportion to the pains he has undergone to gain them. \n\n - Cyrus the Great"
 
 /*
 Prints out a famous quote into http response.
