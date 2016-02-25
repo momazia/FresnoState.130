760593877dc879edbf3a7316b7ecde30cf397673
momazia@gmail.com <momazia@gmail.com>
2016-02-25 15:20:13 -0800

    AV Exercise.

---
+++ src/exercise/urlpath/main.go
@@ -0,0 +1,31 @@
+/*
+	Source: 		Column AV in Gradebook
+	Author: 		Mohamad Mahdi Ziaee
+	Description:	Create a webpage that displays the URL path using req.URL.Path
+*/
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	// Registering the URL path and binding it to surferPage handler
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		io.WriteString(res, "URL:"+req.URL.Path)
+	})
+
+	// Setting the listener on port 8080
+	log.Println("Listening to 8080 ...")
+	http.ListenAndServe(":8080", nil)
+}
+
+// Logs the error given into log
+func logError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
